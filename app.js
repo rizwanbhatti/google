7cf250c09ff3e9b1eb8ad7925775b56ce5219b55
Rizwan Bhatti <rizwanbhatti15@gmail.com>
2025-07-22 19:55:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ async function google() {
   const { data, error } = await client.auth.signInWithOAuth({
   provider: 'google',
   options: {
-    // redirectTo: 'https://example.com/welcome'
+    redirectTo: 'dashboard.html'
   }
 })
 
